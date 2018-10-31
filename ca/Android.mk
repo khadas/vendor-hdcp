a44db0f6d02b9802740762934d51fba284dc76fa
Hangyu Li <hangyu.li@amlogic.com>
2018-10-31 13:26:22 +0800

    hdcp: move hdcp ta/ca/.rc to vendor directory [3/4]
    PD#SWPL-1199
    
    Problem:
    hdcp ta/ca need locate in vendor dir in android treble mode
    
    Solution:
    move hdcp related files to vendor directory
    
    Verify:
    Android P + Ampere
    
    Change-Id: I4b4fdea4edc7027bbc45d778a60ecbd4a5b9035e
    Signed-off-by: Hangyu Li <hangyu.li@amlogic.com>

--- ca/Android.mk
+++ ca/Android.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+OUT_PATH := $(PRODUCT_OUT)/vendor
+else
+OUT_PATH := $(PRODUCT_OUT)/system
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
@@ -10,8 +16,11 @@ LOCAL_SRC_FILES := bin64/tee_hdcp
 endif
 
 LOCAL_INIT_RC := tee_hdcp.rc
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+LOCAL_PROPRIETARY_MODULE := true
+endif
 
 LOCAL_MODULE := tee_hdcp
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/vendor/bin
+LOCAL_MODULE_PATH := $(OUT_PATH)/bin
 include $(BUILD_PREBUILT)
--- ta/Android.mk
+++ ta/Android.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+OUT_PATH := $(PRODUCT_OUT)/vendor
+else
+OUT_PATH := $(PRODUCT_OUT)/system
+endif
+
 include $(CLEAR_VARS)
 ifeq ($(TARGET_ENABLE_TA_SIGN), true)
 ifeq ($(TARGET_ENABLE_TA_ENCRYPT), true)
@@ -7,11 +13,19 @@ ENCRYPT := --encrypt=1
 else
 ENCRYPT := --encrypt=0
 endif
-$(info $(shell mkdir $(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/signed))
-$(info $(shell $(ANDROID_BUILD_TOP)/vendor/amlogic/tdk/ta_export/scripts/sign_ta_auto.py \
-		--in=$(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
-		--out=$(ANDROID_BUILD_TOP)/$(LOCAL_PATH)/signed/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
-		$(ENCRYPT)))
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28 && echo OK),OK)
+KEYDIR := --keydir=$(shell pwd)/$(BOARD_AML_TDK_KEY_PATH)
+else
+KEYDIR := --keydir=$(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/keys/
+endif
+$(info $(shell mkdir $(shell pwd)/$(LOCAL_PATH)/signed))
+$(info $(shell $(shell pwd)/$(BOARD_AML_VENDOR_PATH)/tdk/ta_export/scripts/sign_ta_auto.py \
+		--in=$(shell pwd)/$(LOCAL_PATH)/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
+		--out=$(shell pwd)/$(LOCAL_PATH)/signed/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
+		$(ENCRYPT) \
+		$(KEYDIR)))
+
 LOCAL_SRC_FILES := signed/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta
 else
 LOCAL_SRC_FILES := ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta
@@ -20,7 +34,7 @@ endif
 LOCAL_MODULE := ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .ta
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/lib/teetz
+LOCAL_MODULE_PATH := $(OUT_PATH)/lib/teetz
 LOCAL_STRIP_MODULE := false
 include $(BUILD_PREBUILT)
 
